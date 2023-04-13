7f276f34e65ef3e68639c7a3902b5d7a90e6c958
Shini <halimshini@hotmail.com>
2023-04-13 00:20:19 -0400

    added pugins in webpack.config

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {console.error('putDb not implemented');
+const pwaDb = await openDB('pwa', 1);
+const tx = todosDb.transaction('pwa', 'readwrite');
+const store = tx.objectStore('pwa');
+const request = store.add({ pwa: content });
+const result = await request;
+console.log('🚀 - data saved to the database', result);
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {console.error('getDb not implemented');
+  console.log('GET all from the database');
+  const pwaDb= await openDB('pwa', 1);
+  const tx = todosDb.transaction('pwa', 'readonly');
+  const store = tx.objectStore('pwa');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 initdb();
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,10 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const { InjectManifest,GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -18,12 +15,65 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: ' PWA'
+      }),
+      new GenerateSW(
+        {
+          exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+          runtimeCaching: [{
+            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 8,
+              },
+            },
+          }],
+        }
+      ),
+      new WebpackPwaManifest({
+        // Create a manifest.json:
+        name: 'PWA TEXT EDITOR',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        fingerprints:'false',
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],// multiple sizes
+            destination: path.join('assets', 'icons'),
+          },
+
+        ]
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
